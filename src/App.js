5dd62a09518b3d0a7f2c6328325df0b8e42284bf
Fahd Munir <fahd.munir.89@gmail.com>
2022-03-14 22:39:33 -0400

    routes created pages and headernav component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './styles/App.scss';
+import { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      // Write code here
-    </div>
-  );
+import HeaderNav from './components/HeaderNav/HeaderNav';
+import Inventory from './pages/Inventory/Inventory';
+import Warehouses from './pages/Warehouses/Warehouses';
+
+class App extends Component {
+  render() {
+    return(
+      <BrowserRouter>
+        <div className="App">
+          <HeaderNav />
+          <Switch>
+            <Route path="/" component={Warehouses} exact />
+            <Route path="/warehouses" component={Warehouses} />
+            <Route path="/warehouses/:id" component={Warehouses} />
+            <Route path="/inventory" component={Inventory} />
+            <Route path="/inventory/:id" component={Inventory} />            
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/Warehouses/Warehouses.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import axios from 'axios';
+import './Warehouses.scss';
+
+
+class Warehouses extends Component {
+    render () {
+        return (
+            <>
+            <p>warehouse page</p> 
+            
+            </>
+        );
+    }
+}
+
+export default Warehouses;
