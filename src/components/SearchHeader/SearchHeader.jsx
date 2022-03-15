815ea5dc480244fe896f50561e6a9a5c82a53f41
Elizabete <88399956+elizaspaula@users.noreply.github.com>
2022-03-15 16:29:17 -0700

    Adjust the SearchHeader to use props

--- src/components/SearchHeader/SearchHeader.jsx
+++ src/components/SearchHeader/SearchHeader.jsx
@@ -2,14 +2,14 @@ import "./SearchHeader.scss";
 import MainButton from "../MainButton/MainButton";
 import SearchField from "../SearchField/SearchField";
 
-function SearchHeader() {
+function SearchHeader(props) {
   return (
     <>
       <div className="inventory">
-        <h1 className="inventory__title">Inventory</h1>
+        <h1 className="inventory__title">{props.title}</h1>
         <SearchField />
         <MainButton>
-          <h3 className="inventory__content btn">+ Add New Item</h3>
+          <h3 className="inventory__content btn">{props.content}</h3>
         </MainButton>
       </div>
     </>
