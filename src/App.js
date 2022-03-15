d07b9cc358ed009cfdc5f3c7a8bf693119a13730
Chris Nguyen <chrisngtutor@gmail.com>
2022-03-14 23:15:59 -0700

    finished front-end styling for edit inventory page for mobile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles/App.scss";
 import Header from "./components/Header/Header";
+import EditInven from "./components/EditInven/EditInven";
 function App() {
   return (
     <div className="App">
       <Header />
+      <EditInven />
     </div>
   );
 }
