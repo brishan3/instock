250665e139b264c71996178916f54511270405e6
Fahd Munir <fahd.munir.89@gmail.com>
2022-03-16 02:20:21 -0400

    get all warehouse request working and styling mirrors inventory

--- src/components/WarehouseEntry/WarehouseEntry.js
+++ src/components/WarehouseEntry/WarehouseEntry.js
@@ -2,49 +2,97 @@ import "./WarehouseEntry.scss";
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
 import { Link } from "react-router-dom";
-// where to place key={warehouse.id} for this?
-function WarehouseEntry(props) {
-  return (
-    
-      <>
-        {props.warehouses.map((warehouse) => (
-          <div className="warehouseTable" key={warehouse.id}>
-            <div className="warehouseTable__location">
-              <div className="warehouseTable__warehouse">
-                <h4 className="warehouseTable__header">Warehouse</h4>
-                <Link to="/warehouses/:id" className="warehouseTable__name">
-                  {warehouse.name}
-                </Link>
-              </div>
-              <div className="warehouseTable__address">
-                <h4 className="warehouseTable__header">Address</h4>
-                <p className="body-medium">
-                  {warehouse.address},
-                  {warehouse.city},
-                  {warehouse.country}
-                </p>
-              </div>
-              <img className="delete-icon" src={deleteIcon} alt="delete-icon" />
-            </div>
+//import { v4 as uuid } from "uuid";
 
-            <div className="warehouseTable__contact">
-              <div className="warehouseTable__contactName">
-                <h4 className="warehouseTable__header">Contact Name</h4>
-                <p className="body-medium">{warehouse.contact.name}</p>
+function WarehouseEntry({ warehouses }) {
+  return (    
+      <div className="wrapper">
+        <ul className="warehouses body-medium">
+          {warehouses.map((warehouse) => (
+            <li className="warehouses__list" key={warehouse.id}>
+              <div className="warehouses__wrapper">
+                <div className="warehouses__content">
+                  <h4 className="warehouses__header">Warehouse</h4>
+                  <Link to="/warehouses/:id" className="warehouses__description">
+                   {warehouse.name}
+                  </Link>
+                </div>
+                <div className="warehouses__content">
+                  <h4 className="warehouses__header">Contact Name</h4>
+                  <p className="warehouses__contactName">{warehouse.contact.name}</p>
+                </div>               
               </div>
-              <div>
-                <h4 className="warehouseTable__header">Contact Infomration</h4>
-                <p className="body-medium"> {warehouse.contact.phone}</p>
-                <p className="body-medium"> {warehouse.contact.email}</p>
+              <div className="warehouses__wrapper">
+                <div className="warehouses__content">
+                  <h4 className="warehouses__header">Address</h4>
+                  <p className="warehouses__address">
+                   {warehouse.address},
+                   {warehouse.city},
+                   {warehouse.country}
+                  </p>
+                </div>
+                <div className="warehouses__content">
+                  <h4 className="warehouses__header">Contact Infomration</h4>
+                  <p className="warehouses__phone"> {warehouse.contact.phone}</p>
+                  <p className="warehouses__email"> {warehouse.contact.email}</p>
+                </div>
               </div>
-              <img className="edit-icon" src={editIcon} alt="edit-icon" />
-            </div>
-          </div>
-        ))}
-      </>
-      
-    
+              <div className="warehouses__buttons">
+                <button className="warehouses__button">
+                  <img 
+                    className="delete-icon" 
+                    src={deleteIcon} 
+                    alt="delete-icon" 
+                  />
+                </button>
+                <button className="warehouses_buttons">
+                  <img 
+                    className="edit-icon" 
+                    src={editIcon} 
+                    alt="edit-icon" 
+                  />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
   );
 }
 
 export default WarehouseEntry;
+
+
+        //   <div className="warehouseTable" key={warehouse.id}>
+        //     <div className="warehouseTable__location">
+        //       <div className="warehouseTable__warehouse">
+        //         <h4 className="warehouseTable__header">Warehouse</h4>
+        //         <Link to="/warehouses/:id" className="warehouseTable__name">
+        //           {warehouse.name}
+        //         </Link>
+        //       </div>
+        //       <div className="warehouseTable__address">
+        //         <h4 className="warehouseTable__header">Address</h4>
+        //         <p className="body-medium">
+        //           {warehouse.address},
+        //           {warehouse.city},
+        //           {warehouse.country}
+        //         </p>
+        //       </div>
+        //       <img className="delete-icon" src={deleteIcon} alt="delete-icon" />
+        //     </div>
+
+        //     <div className="warehouseTable__contact">
+        //       <div className="warehouseTable__contactName">
+        //         <h4 className="warehouseTable__header">Contact Name</h4>
+        //         <p className="body-medium">{warehouse.contact.name}</p>
+        //       </div>
+        //       <div>
+        //         <h4 className="warehouseTable__header">Contact Infomration</h4>
+        //         <p className="body-medium"> {warehouse.contact.phone}</p>
+        //         <p className="body-medium"> {warehouse.contact.email}</p>
+        //       </div>
+        //       <img className="edit-icon" src={editIcon} alt="edit-icon" />
+        //     </div>
+        //   </div>
+        // ))}
--- src/pages/Warehouses/Warehouses.js
+++ src/pages/Warehouses/Warehouses.js
@@ -2,54 +2,64 @@ import { Component } from 'react';
 import axios from 'axios';
 import './Warehouses.scss';
 
-import SearchField from '../../components/SearchField/SearchField';
-import MainButton from '../../components/MainButton/MainButton';
-import WarehousesTable from '../../components/WarehousesTable/WarehousesTable';
+import WarehouseEntry from '../../components/WarehouseEntry/WarehouseEntry';
+import SearchHeader from '../../components/SearchHeader/SearchHeader';
+
+// import SearchField from '../../components/SearchField/SearchField';
+// import MainButton from '../../components/MainButton/MainButton';
+// import WarehousesTable from '../../components/WarehousesTable/WarehousesTable';
 
 
 class Warehouses extends Component {
     state = {
         warehouses: [],
-        selectedWarehouse: {},
     };
 
     //fetching API data and setting state when it mounts for the first time
     componentDidMount() {
-        this.getWarehousesData();
+        this.getAllWarehouses();
     }
 
-    getWarehousesData(){
-        axios.get(`${process.env.REACT_APP_API_URL}`)
+    getAllWarehouses(){
+
+        axios.get(`${process.env.REACT_APP_API_URL}/warehouses`)
         .then((response) => {
             this.setState({
                 warehouses:response.data,
-            })
+            });
         })
         .catch((error) => {
             console.log(error);
-        })
+        });
     }
 
     render () {
         return (
-            <div className="warehousesPage">
-                <div className="warehousesHeader">
-                    <h1 className="warehousesHeader__title">Warehouses</h1>
-                    <SearchField />
-                    <MainButton>
-                        <h3 className="warehousesHeader__button">+ Add New Warehouse</h3>
-                    </MainButton>                
-                </div>
-                <WarehousesTable 
-                    warehouses={this.state.warehouses}
-                />
-                   
-            </div>
+            <>
+                <SearchHeader title="Warehouses" content="+ add new warehouse"/>
+                <WarehouseEntry warehouses={this.state.warehouses} />
+            </>
         );
     }
 }
 
 export default Warehouses;
+
+/* <div className="warehousesPage">
+<div className="warehousesHeader">
+    <h1 className="warehousesHeader__title">Warehouses</h1>
+    <SearchField />
+    <MainButton>
+        <h3 className="warehousesHeader__button">+ Add New Warehouse</h3>
+    </MainButton>                
+</div>
+<WarehousesTable 
+    warehouses={this.state.warehouses}
+/>
+   
+</div> */
+
+
                     /* <form className="warehousesForm">
                         <button type="submit" className="warehousesForm__btn">
                             <img className="warehousesForm__searchIcon" src={this.searchIcon} />
