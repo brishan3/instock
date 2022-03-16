45ebba96fc107c91f045b8a348446774e4803803
Elizabete <88399956+elizaspaula@users.noreply.github.com>
2022-03-16 08:14:32 -0700

    Create link in the inventory page to the add inventory

--- src/components/MainButton/MainButton.jsx
+++ src/components/MainButton/MainButton.jsx
@@ -1,7 +1,8 @@
 import "./MainButton.scss";
+import { Link } from "react-router-dom";
 
 function MainButton(props) {
-  return <button className="button__btn btn" type="submit" {...props}></button>;
+  return <Link className="button__btn btn" type="submit" {...props}></Link>;
 }
 
 export default MainButton;
