49521480d326dd64f51d27edf614f731e7856fe9
Elizabete <88399956+elizaspaula@users.noreply.github.com>
2022-03-16 19:00:40 -0700

    Create inventory list for tablet and desktop

--- src/components/InventoryList/InventoryList.jsx
+++ src/components/InventoryList/InventoryList.jsx
@@ -3,10 +3,10 @@ import deleteIcon from "../../assets/icons/delete_outline-24px.svg";
 import editIcon from "../../assets/icons/edit-24px.svg";
 import { v4 as uuid } from "uuid";
 
-function InventoryEntry({ inventories }) {
+function InventoryList({ inventories }) {
   return (
-    <div className="wrapper">
-      <ul className="items body-medium">
+    <>
+      <ul className="items body-medium  mobile">
         {inventories.map((inventory) => (
           <li className="items__list" key={uuid()}>
             <div className="items__wrapper">
@@ -23,15 +23,7 @@ function InventoryEntry({ inventories }) {
                       : "items__red"
                   }`}
                 >
-                  <h3
-                    className={`items__status ${
-                      inventory.status === "In Stock"
-                        ? "items__green"
-                        : "items__red"
-                    }`}
-                  >
-                    {inventory.status}
-                  </h3>
+                  <h3 className="items__status">{inventory.status}</h3>
                 </div>
               </div>
             </div>
@@ -67,8 +59,50 @@ function InventoryEntry({ inventories }) {
           </li>
         ))}
       </ul>
-    </div>
+
+      <table className="table body-medium tablet-desktop">
+        <tr className="table__head">
+          <th className="table__header">inventory item</th>
+          <th className="table__header">category</th>
+          <th className="table__header">status</th>
+          <th className="table__header">qtd</th>
+          <th className="table__header">warehouse</th>
+          <th className="table__header">actions</th>
+        </tr>
+        {inventories.map((inventory) => (
+          <tr className="table__row" key={uuid}>
+            <td className="table__data">{inventory.itemName}</td>
+            <td className="table__data">{inventory.category}</td>
+            <td className="table__data">
+              <div
+                className={`table__tag ${
+                  inventory.status === "In Stock"
+                    ? "table__green"
+                    : "table__red"
+                }`}
+              >
+                <h3 className="table__status">{inventory.status}</h3>
+              </div>
+            </td>
+            <td className="table__data">{inventory.quantity}</td>
+            <td className="table__data">{inventory.warehouseName}</td>
+            <td className="table__data icons">
+              <button className="items__button">
+                <img
+                  className="items__icon"
+                  src={deleteIcon}
+                  alt="delete-icon"
+                />
+              </button>
+              <button className="items__button">
+                <img className="items__icon" src={editIcon} alt="edit-icon" />
+              </button>
+            </td>
+          </tr>
+        ))}
+      </table>
+    </>
   );
 }
 
-export default InventoryEntry;
+export default InventoryList;
