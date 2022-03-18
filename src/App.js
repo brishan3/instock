69eabdcb002421f325299481c4f3da1e9a56d2b4
Elizabete <88399956+elizaspaula@users.noreply.github.com>
2022-03-17 17:06:22 -0700

    Adjust edit, item links. Create route inventory/id

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import EditInven from "./components/EditInven/EditInven";
 import AddInven from "./components/AddInven/AddInven";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import AddWarehouse from './components/AddWarehouse/AddWarehouse.jsx';
+import AddWarehouse from "./components/AddWarehouse/AddWarehouse.jsx";
 import EditWarehouse from "./components/EditWarehouse/EditWarehouse.jsx";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
@@ -21,10 +21,15 @@ function App() {
             <Switch>
               <Route path="/" component={Warehouses} exact />
               <Route path="/warehouses" exact component={Warehouses} />
-              <Route path="/warehouses/edit/:id" exact component={EditWarehouse} />
+              <Route
+                path="/warehouses/edit/:id"
+                exact
+                component={EditWarehouse}
+              />
               <Route path="/warehouses/add" exact component={AddWarehouse} />
               <Route path="/warehouses/:id" component={WarehouseDetails} />
               <Route path="/inventory" exact component={Inventory} />
+              {/* <Route path="/inventory/:id" exact component={InventoryDetails} /> */}
               <Route path="/inventory/add" component={AddInven} />
               <Route path="/inventory/edit/:id" component={EditInven} />
             </Switch>
